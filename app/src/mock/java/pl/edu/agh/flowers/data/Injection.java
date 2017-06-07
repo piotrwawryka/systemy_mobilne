1f0b8622a30c80af09c51c8f7633f03b38702863
Kamil Kulak <kulak.kam@gmail.com>
2017-06-07 21:53:01 +0200

    Repackage, removed redundant classes

--- app/src/mock/java/pl/edu/agh/flowers/data/Injection.java
+++ app/src/mock/java/pl/edu/agh/flowers/data/Injection.java
@@ -14,26 +14,28 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp;
+package pl.edu.agh.flowers.data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import pl.edu.agh.flowers.data.source.local.TasksLocalDataSource;
+import pl.edu.agh.flowers.data.FakeTasksRemoteDataSource;
 import pl.edu.agh.flowers.data.source.TasksDataSource;
 import pl.edu.agh.flowers.data.source.TasksRepository;
+import pl.edu.agh.flowers.data.source.local.TasksLocalDataSource;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * Enables injection of production implementations for
- * {@link TasksDataSource} at compile time.
+ * Enables injection of mock implementations for
+ * {@link TasksDataSource} at compile time. This is useful for testing, since it allows us to use
+ * a fake instance of the class to isolate the dependencies and run a test hermetically.
  */
 public class Injection {
 
     public static TasksRepository provideTasksRepository(@NonNull Context context) {
         checkNotNull(context);
-        return TasksRepository.getInstance(TasksRemoteDataSource.getInstance(),
+        return TasksRepository.getInstance(FakeTasksRemoteDataSource.getInstance(),
                 TasksLocalDataSource.getInstance(context));
     }
 }
